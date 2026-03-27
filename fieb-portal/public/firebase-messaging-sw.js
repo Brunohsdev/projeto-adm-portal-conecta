f93d5324e8166cf7d87b095cc69a3defd91cff56
Bruno Santos <bhas795002@gmail.com>
2026-03-27 00:17:27 +0000

    trocando dados do sdk

--- fieb-portal/public/firebase-messaging-sw.js
+++ fieb-portal/public/firebase-messaging-sw.js
@@ -2,12 +2,20 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js')
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBlS5n9YbQBGKibHc2kyeJCDluZFqgeUKI",
-  authDomain: "portal-conecta-890fb.firebaseapp.com",
-  projectId: "portal-conecta-890fb",
-  storageBucket: "portal-conecta-890fb.firebasestorage.app",
-  messagingSenderId: "9058505397",
-  appId: "1:9058505397:web:2c110fdea573a15fb8a4fc"
+
+    apiKey: "AIzaSyBOgqjd4YHXObgFdGz5apgovRLtOU6fkXU",
+  authDomain: "portalconecta-a31df.firebaseapp.com",
+  projectId: "portalconecta-a31df",
+  storageBucket: "portalconecta-a31df.firebasestorage.app",
+  messagingSenderId: "13539357652",
+  appId: "1:13539357652:web:884aa5312e06af34de1c41",
+  measurementId: "G-NHP4X4SKXX"
+  // apiKey: "AIzaSyBlS5n9YbQBGKibHc2kyeJCDluZFqgeUKI",
+  // authDomain: "portal-conecta-890fb.firebaseapp.com",
+  // projectId: "portal-conecta-890fb",
+  // storageBucket: "portal-conecta-890fb.firebasestorage.app",
+  // messagingSenderId: "9058505397",
+  // appId: "1:9058505397:web:2c110fdea573a15fb8a4fc"
 })
 
 const messaging = firebase.messaging()
